58da577627a01c5cb958aedbaab0ce608056b08b
Gregor F <gafrelek@gmail.com>
2024-05-11 00:03:19 +0200

    edited hero

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Features from './components/Features.jsx';
-import Testimonial from './components/Testimonial';
-import CallToAction from './components/CallToAction';
-import Footer from './components/Footer';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Features from "./components/Features.jsx";
+import Testimonial from "./components/Testimonial";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Hero />
-      <Features />
-      <Testimonial />
-      <CallToAction />
+      {/* <Features /> */}
+      {/* <Testimonial /> */}
       <Footer />
     </BrowserRouter>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,33 +1,26 @@
-import { Link } from 'react-router-dom';
-
-import illustrationIntro from '../assets/images/illustration-intro.svg';
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <section id='hero'>
+    <section id="hero" className="hero">
       {/* Flex Container */}
-      <div className='container flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row'>
+      <div className="container h-screen flex flex-col px-6 mx-auto  space-y-0 md:space-y-0 md:flex-row ">
         {/* Left Item */}
-        <div className='flex flex-col mb-32 space-y-12 md:w-1/2'>
-          <h1 className='max-w-md text-4xl font-bold text-center md:text-5xl md:text-left'>
+        <div className="flex flex-col flex-start mt-64 space-y-12 md:w-1/2">
+          <h1 className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left ">
             Bring everyone together to build better products
           </h1>
-          <p className='max-w-sm text-center text-darkGrayishBlue md:text-left'>
+          <p className="text-center text-veryDarkBlue md:text-left lg:pr-44 md:pl-0 sm:pt-20 sm:px-10 ">
             Manage makes it simple for software teams to plan day-to-day tasks
             while keeping the larger team goals in view.
           </p>
-          <div className='flex justify-center md:justify-start'>
+          <div className="flex justify-center md:justify-start">
             <Link
-              to='#'
-              className='p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight'
-            >
+              to="#"
+              className="p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight">
               Get Started
             </Link>
           </div>
-        </div>
-        {/* Image */}
-        <div className='md:w-1/2'>
-          <img src={illustrationIntro} alt='' />
         </div>
       </div>
     </section>
